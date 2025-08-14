e13300a5a17f790697eed46fccccff84d6dea0ea
Lavosier <lavosierbarbosa@gmail.com>
2025-08-14 10:38:19 -0300

    first commit

---
+++ src/main/java/dev/java10X/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10X.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    //@GetMapping("localhost:8080/boasVindas") nao é necessario utilizar o local host para a rota da controller
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Minha primeira menssagem";
+    }
+}
